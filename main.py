1a3efba296c7e35de48727e91eed6b088a67797c
Danilka06 <main@0x1f44.ru>
2024-02-08 18:47:14 +0300

    task 1 class 2

--- main.py
+++ main.py
@@ -13,5 +13,10 @@ def index():
     return "И на Марсе будут яблони цвести!"
 
 
+@app.route("/promotion", methods=["GET"])
+def promotion():
+    return render_template("index.html")
+
+
 if __name__ == '__main__':
     app.run("127.0.0.1", port=8080, debug=True)
